2cd174cab79821932509d9db56d624a55099a3ec
shashwatjv <shashwat@pdx.edu>
2018-03-23 10:06:27 -0700

    basic motor control files

---
+++ dxbasic_ping.py
@@ -0,0 +1,14 @@
+﻿from pyax12.connection import Connection
+
+# Connect to the serial port
+serial_connection = Connection(port="COM5", baudrate=1000000)
+
+for dynamixel_id in range(1, 11) :
+
+	# Ping the third dynamixel unit
+	is_available = serial_connection.ping(dynamixel_id)
+
+	print(is_available)
+
+# Close the serial connection
+serial_connection.close()
---
+++ dxbasic_try.py
@@ -0,0 +1,49 @@
+from pyax12.connection import Connection
+import time
+
+# Connect to the serial port
+serial_connection = Connection(port="COM5", baudrate=1000000)
+
+dynamixel_id = 1
+
+# Go to 0°
+serial_connection.goto(dynamixel_id, 0, speed=112, degrees=True)
+time.sleep(1)    # Wait 1 second
+
+# Go to -45° (45° CW)
+serial_connection.goto(dynamixel_id, -60, speed=512, degrees=True)
+time.sleep(1)    # Wait 1 second
+"""
+# Go to -90° (90° CW)
+serial_connection.goto(dynamixel_id, -60, speed=512, degrees=True)
+time.sleep(1)    # Wait 1 second
+
+# Go to -135° (135° CW)
+serial_connection.goto(dynamixel_id, -135, speed=512, degrees=True)
+time.sleep(1)    # Wait 1 second
+
+# Go to -150° (150° CW)
+serial_connection.goto(dynamixel_id, -150, speed=512, degrees=True)
+time.sleep(1)    # Wait 1 second
+
+# Go to +150° (150° CCW)
+serial_connection.goto(dynamixel_id, 150, speed=512, degrees=True)
+time.sleep(2)    # Wait 2 seconds
+
+# Go to +135° (135° CCW)
+serial_connection.goto(dynamixel_id, 135, speed=512, degrees=True)
+time.sleep(1)    # Wait 1 second
+
+# Go to +90° (90° CCW)
+serial_connection.goto(dynamixel_id, 30, speed=512, degrees=True)
+time.sleep(1)    # Wait 1 second
+"""
+# Go to +45° (45° CCW)
+serial_connection.goto(dynamixel_id, 45, speed=112, degrees=True)
+time.sleep(1)    # Wait 1 second
+
+# Go back to 0°
+serial_connection.goto(dynamixel_id, 0, speed=112, degrees=True)
+
+# Close the serial connection
+serial_connection.close()
