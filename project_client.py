a5d1f5a9cb6fa65397a39d5f070c43594d077b6a
sandeep <cvs.sandeep@gmail.com>
2018-03-24 03:09:11 +0000

    new server/client files

--- project_client.py
+++ project_client.py
@@ -2,13 +2,10 @@ import requests
 import json
 import cv2
 
-cam = cv2.VideoCapture(0)
 while True:
-    ret, im =cam.read()
     url = "192.168.1.4:5000/data"
-    payload = {'the_file':im}
-    r = requests.post(url, data=payload)
-    if r.text['Name'] ==None:
+    r = requests.get(url)
+    if r.text['Name'] == None:
         pass
     else:
         if r.text['Name'] == "<Insert name and motion>"
